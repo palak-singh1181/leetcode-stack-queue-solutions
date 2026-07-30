698ee8621c1c66004c9edce936ce30645720da94
Palak Singh <palaksingh9503@gmail.com>
2026-07-30 03:15:13 -0700

    Added this solution of leetcode

---
+++ Medium/FinalArrayStateAfterKMultiplicationOperations.java
@@ -0,0 +1,35 @@
+
+import java.util.Arrays;
+
+public class FinalArrayStateAfterKMultiplicationOperations {
+
+    public static int[] getFinalState(int[] nums, int k, int multiplier) {
+
+        while (k > 0) {
+
+            int minIndex = 0;
+
+            for (int i = 1; i < nums.length; i++) {
+                if (nums[i] < nums[minIndex]) {
+                    minIndex = i;
+                }
+            }
+
+            nums[minIndex] = nums[minIndex] * multiplier;
+            k--;
+        }
+
+        return nums;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {2, 1, 3, 5, 6};
+        int k = 5;
+        int multiplier = 2;
+
+        int[] result = getFinalState(nums, k, multiplier);
+
+        System.out.println(Arrays.toString(result));
+    }
+}
