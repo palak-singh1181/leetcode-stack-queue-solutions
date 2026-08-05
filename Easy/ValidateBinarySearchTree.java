ff1d93a9d4a2a777061336248e481de46d01e95d
Palak Singh <palaksingh9503@gmail.com>
2026-08-05 09:59:55 -0700

    Added this solution of leetcode

---
+++ Easy/ValidateBinarySearchTree.java
@@ -0,0 +1,37 @@
+
+public class ValidateBinarySearchTree {
+
+    static class TreeNode {
+        int val;
+        TreeNode left, right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public static boolean isValidBST(TreeNode root) {
+        return validate(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    private static boolean validate(TreeNode node, long min, long max) {
+
+        if (node == null)
+            return true;
+
+        if (node.val <= min || node.val >= max)
+            return false;
+
+        return validate(node.left, min, node.val) &&
+               validate(node.right, node.val, max);
+    }
+
+    public static void main(String[] args) {
+
+        TreeNode root = new TreeNode(2);
+        root.left = new TreeNode(1);
+        root.right = new TreeNode(3);
+
+        System.out.println(isValidBST(root)); // true
+    }
+}
