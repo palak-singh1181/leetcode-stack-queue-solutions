7b09b3558bf1ed58fe6b268ab5e8ced8547e24c1
Palak Singh <palaksingh9503@gmail.com>
2026-08-06 05:45:03 -0700

    Added this solution of leetcode

---
+++ Medium/ConvertSortedArrayToBinarySearchTree.java
@@ -0,0 +1,52 @@
+
+public class ConvertSortedArrayToBinarySearchTree {
+
+    static class TreeNode {
+        int val;
+        TreeNode left, right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public static TreeNode sortedArrayToBST(int[] nums) {
+        return build(nums, 0, nums.length - 1);
+    }
+
+    private static TreeNode build(int[] nums, int left, int right) {
+
+        if (left > right)
+            return null;
+
+        int mid = left + (right - left) / 2;
+
+        TreeNode root = new TreeNode(nums[mid]);
+
+        root.left = build(nums, left, mid - 1);
+        root.right = build(nums, mid + 1, right);
+
+        return root;
+    }
+
+    // Inorder Traversal
+    public static void inorder(TreeNode root) {
+
+        if (root == null)
+            return;
+
+        inorder(root.left);
+        System.out.print(root.val + " ");
+        inorder(root.right);
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {-10, -3, 0, 5, 9};
+
+        TreeNode root = sortedArrayToBST(nums);
+
+        System.out.print("Inorder Traversal: ");
+        inorder(root);
+    }
+}
